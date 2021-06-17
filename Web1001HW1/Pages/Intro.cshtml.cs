bc3c67fd59301b96cd3e145c85c9b4203dca34cd
NavalSharma7 <A00241484@myCambrian.ca>
2021-06-18 04:45:48 +0530

    * Intro.cshtml: * Intro.cshtml.cs:
    * _Layout.cshtml: added two number input and an output with submit
      button which shows the result of multiplication of the two inputs

---
+++ Web1001HW1/Pages/Intro.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web1001HW1.Pages
+{
+    public class IntroModel : PageModel
+    {
+        public void OnGet()
+        {
+            if (firstNo > 0 && secondNo > 0)
+            {
+                result = firstNo * secondNo;
+                resultSet = true;
+            }
+        }
+
+        [FromQuery(Name = "firstNo")]
+
+        public int firstNo { get; set; }
+
+        [FromQuery(Name = "secondNo")]
+        public int secondNo{ get; set; }
+
+
+        public int result { get; set; }
+
+        public bool resultSet { get; set; }
+    }
+}
